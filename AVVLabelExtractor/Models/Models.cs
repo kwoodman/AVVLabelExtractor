b102963ec9cf9c8e9906c96ec18d7cbdfcf45225
Kelvin K. Woodman <kwoodman@gmail.com>
2022-09-29 08:55:44 -0400

    Add project files.

---
+++ AVVLabelExtractor/Models/Models.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+namespace AVV_Label_Extractor.Models
+{
+    public class TreeItemData
+    {
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public XDocument FileContent { get; set; }
+
+        public bool IsExpanded { get; set; }
+
+        public HashSet<TreeItemData> TreeItems { get; set; }
+
+        public TreeItemData(string title, string icon, XDocument? filecontent = null)
+        {
+            Title = title;
+            Icon = icon;
+            FileContent = filecontent;
+        }
+    }
+}
